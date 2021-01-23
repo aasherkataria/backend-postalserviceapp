26285089ad2e5d3f84fdda71aa241bef88426258
aasherkataria <aasherkataria@outlook.com>
2021-01-23 00:59:22 -0600

    updated filename and route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const parcelRoute = require('./routes/locationInfo');
+const parcelRoute = require('./routes/parcelRoute');
 
 
 app.use('/parcel', parcelRoute);
