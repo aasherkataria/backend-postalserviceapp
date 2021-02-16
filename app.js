13b68017895d44728548fa77e3ec9b5d000354f2
aasherkataria <aasherkataria@outlook.com>
2021-02-15 22:54:14 -0600

    update port

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const parcelRoute = require('./routes/parcelRoute');
 
 app.use('/employee/parcel', parcelRoute);
 
-app.listen('3000', () => {
-    console.log('server started on port 3000');
-}); 
+app.set('port', process.env.PORT || 3000);
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+// Only works on 3000 regardless of what I set environment port to or how I set
+// [value] in app.set('port', [value]).
+// app.listen(3000);
+app.listen(app.get('port'));
